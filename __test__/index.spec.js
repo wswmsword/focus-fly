4d8f8a5002631532b336ff15617d906a05aa150e
wswmsword <cjzjzzz@gmail.com>
2023-04-09 09:21:39 +0800

    extract testing html string

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -13,33 +13,10 @@ import focusBagel from '../index.js';
 import userEvent from '@testing-library/user-event';
 import render from './helper/render.js';
 const user = userEvent.setup();
+import modalHtml from './template-html/modal.js';
 
 function getModalDom() {
-  const modal = `
-    <button id="open">打开一包糖</button>
-    <button id="walk1">走两步</button>
-    <div
-      id="dialog"
-      aria-modal="true"
-      aria-labelledby="modal-title"
-      class="centre"
-      role="dialog"
-      tabindex="0">
-      <h2 id="modal-title">吃一颗糖</h2>
-      <ul>
-        <li><a id="firstFocusA" href="#">椰蓉酥心糖</a></li>
-        <li><a href="#">不二家棒棒糖</a></li>
-        <li><a href="#">樱花软糖</a></li>
-        <li><a href="#">大白兔奶糖</a></li>
-        <li><a href="#">夹心巧克力</a></li>
-      </ul>
-      <div class="ctrls">
-        <button id="close">Close</button>
-        <button id="lastFocusBtn">OK</button>
-      </div>
-    </div>
-    <button id="walk2">走两步</button>
-  `;
+  const modal = modalHtml;
 
   const { container } = render(modal);
 
@@ -141,5 +118,13 @@ describe("focus-bagel", function() {
     expect(getByText(container, "不二家棒棒糖")).toHaveFocus();
     await user.tab(s);
     expect(first).toHaveFocus();
+  });
+
+  test("should enter by return api", function() {
+    
+  });
+
+  test("should exit by return api", function() {
+
   });
 });
