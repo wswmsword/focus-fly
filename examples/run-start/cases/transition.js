26cf33ec7db4c992b98713ca150e18fc42085fd6
wswmsword <cjzjzzz@gmail.com>
2023-04-14 18:59:53 +0800

    add example(transition)

---
+++ examples/run-start/cases/transition.js
@@ -0,0 +1,37 @@
+const dialog4 = document.getElementById("dialog_4");
+const first4 = document.getElementById("firstFocusA_4");
+const last4 = document.getElementById("lastFocusBtn_4");
+const open4 = document.getElementById("open_4");
+const close4 = document.getElementById("close_4");
+
+let exitTransition = false;
+
+const bagel_4 = focusBagel(dialog4, [first4, last4], {
+  enter: {
+    selector: open4,
+    on() {
+      exitTransition = false;
+      dialog4.classList.add("openedDialog");
+      dialog4.classList.remove("closedDialog");
+      setTimeout(() => {
+        dialog4.classList.remove("transition-out");
+        dialog4.classList.add("transition-in");
+      }, 0);
+    },
+  },
+  exit: {
+    selector: close4,
+    on() {
+      exitTransition = true;
+      dialog4.classList.remove("transition-in");
+      dialog4.classList.add("transition-out");
+    },
+  },
+});
+
+dialog4.addEventListener("transitionend", () => {
+  if (exitTransition) {
+    dialog4.classList.remove("openedDialog");
+    dialog4.classList.add("closedDialog");
+  }
+});
