b9f3800c191a1f811c09dbdb6a6a929fb958a5f8
wswmsword <cjzjzzz@gmail.com>
2023-04-16 11:43:13 +0800

    update typings

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,108 @@ type handleKeydown = (e: KeyboardEvent) => any;
 
 type element = string | Element | HTMLElement;
 
+type isKey = (e: KeyboardEvent) => boolean;
+
+type subNodesForward = {
+  /** 自定义前进 subNodes 的组合键 */
+  key?: isKey;
+
+  /** 前进时的行为 */
+  on?: handleKeydown;
+};
+
+type subNodesBackward = {
+  /** 自定义后退 subNodes 的组合键 */
+  key?: isKey;
+
+  /** 后退时的行为 */
+  on?: handleKeydown;
+};
+
+type subNodesHeadPrev = {
+/** subNodes 的前一个可 tab 的元素 */
+  node?: element;
+
+  /** 自定义聚焦该元素的组合键 */
+  key?: isKey;
+
+  /** 聚焦时的行为 */
+  on?: handleKeydown;
+};
+
+type subNodesTailNext = {
+  /** subNodes 的后一个可 tab 的元素 */
+  node?: element;
+
+  /** 自定义聚焦该元素的组合键 */
+  key?: isKey;
+
+  /** 聚焦时的行为 */
+  on?: handleKeydown;
+};
+
+type enterSubNodes = {
+  /** 触发器，将用于监听点击事件，用于退出焦点循环时聚焦使用 */
+  node?: element;
+
+  /** 自定义进入 subNodes 组合键 */
+  key?: iskey;
+
+  /** 点击触发器后的行为 */
+  on?: handleKeydown;
+}
+
+type exitSubNodes = {
+
+  /** 退出循环焦点的触发器，用于监听点击事件 */
+  node?: element;
+
+  /** 自定义退出 subNodes 组合键 */
+  key?: iskey;
+
+  /** 点击退出循环焦点的触发器后的行为 */
+  on?: handleKeydown;
+};
+
+type cover = {
+  /** 封面元素 */
+  node?: element;
+
+  /** 封面的后一个可 tab 的元素 */
+  next?: element;
+
+  /** 自定义聚焦封面后面元素的组合键 */
+  nextKey?: isKey;
+
+  /** 聚焦后面可 tab 元素的行为 */
+  onNext?: handleKeydown;
+
+  /** 封面的前一个可 tab 的元素 */
+  prev?: element;
+
+  /** 自定义聚焦封面前面元素的组合键 */
+  prevKey?: isKey;
+
+  /** 聚焦前面可 tab 元素的行为 */
+  onPrev?: handleKeydown;
+
+  /** 自定义进入 subNodes 的组合键 */
+  enterKey?: isKey;
+
+  /** 进入 subNodes 时的行为 */
+  onEnter?: handleKeydown;
+
+  /** 自定义退出封面的组合键 */
+  exitKey?: isKey;
+
+  /** 退出封面时的行为 */
+  onExit?: handleKeydown;
+};
+
+type promiseDelay = () => Promise<unknown>;
+
+type callbackDelay = (fn: () => any) => any;
+
 interface Options {
 
   /** 是否指定聚焦的元素，设置 true 则按顺序聚焦 `subNodes` */
@@ -11,34 +113,37 @@ interface Options {
   loop?: boolean;
 
   /** 自定义*前进*焦点函数，设置后，`manual` 将默认为 true */
-  isForward?: (e: KeyboardEvent) => boolean;
+  forward?: isForward | subNodesForward;
 
   /** 自定义*后退*焦点函数，设置后，`manual` 将默认为 true */
-  isBackward?: (e: KeyboardEvent) => boolean;
+  backward?: isBackward | subNodesBackward;
+
+  /** 聚焦 subNodes 的后一个元素 */
+  headPrev?: subNodesHeadPrev;
+
+  /** 聚焦 subNodes 的前一个元素 */
+  tailNext?: subNodesTailNext;
 
   /** 触发器，用于退出焦点循环时聚焦使用，如果在其它地方设置，可以忽略，例如设置 `enter.selector` 后，不用设置 `trigger` */
   trigger?: element;
 
-  enter?: {
+  /** 进入 subNodes */
+  enter?: enterSubNodes;
 
-      /** 触发器，将用于监听点击事件，用于退出焦点循环时聚焦使用 */
-      selector?: element;
-
-      /** 点击触发器后的行为 */
-      on?: handleKeydown;
-  };
-
-  exit?: {
-
-      /** 退出循环焦点的触发器，用于监听点击事件 */
-      selector?: element;
-
-      /** 点击退出循环焦点的触发器后的行为 */
-      on?: handleKeydown;
-  };
+  /** 退出 subNodes */
+  exit?: exitSubNodes;
 
   /** 按下 `esc` 的行为，如果未设置，默认取 `exit.on` */
   onEscape?: false | handleKeydown;
+
+  /** 封面相关 */
+  cover?: cover;
+
+  /** 延迟聚焦，触发 trigger 后等待执行 delayToFocus 完成后聚焦 */
+  delayToFocus?: promiseDelay | callbackDelay;
+
+  /** 每次退出至 trigger 是否移除事件 */
+  removeListenersEachExit?: boolean;
 }
 
 interface Return {
@@ -56,3 +161,6 @@ interface Return {
 declare const focusBagel: (rootNode: element, subNodes: element[], options?: Options) => Return;
 
 export default focusBagel;
+
+// export function focusBagel(rootNode: element, subNodes: element[], options?: Options): Return;
+// export function focusBagel(subNodes: element[], options?: Options): Return;
