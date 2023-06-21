d50397bc1842d5d49d39b4a42eeb07cd2df4c13f
wswmsword <cjzjzzz@gmail.com>
2023-06-21 14:09:10 +0800

    update example

--- examples/run-start/missions/dialog.js
+++ examples/run-start/missions/dialog.js
@@ -1,9 +1,9 @@
-
 const dialog = document.getElementById("dialog");
 const mask = document.getElementById("dialog_mask");
 const entry = document.getElementById("open");
 
-const bagel = focusBagel(dialog, ["#firstFocusBtn", "#close"], {
+// 6～18 行为焦点管理的部分，管理了焦点的入口、出口，以及焦点在列表内移动的范围
+focusBagel(dialog, ["#firstFocusBtn", "#close"], { // L:6
   loop: false,
   entry: {
     node: entry,
@@ -15,18 +15,22 @@ const bagel = focusBagel(dialog, ["#firstFocusBtn", "#close"], {
     type: ["click", "outlist"]
   },
   onEscape: true,
-});
+}); // L:18
 
+// 下面的两个函数和焦点无关，和样式或其它逻辑有关，这些代码在实际开发中，可以和上面的焦点部分分开，或者可以像本例中，把这些代码集成到焦点管理中
+
+/** 打开弹窗，设置打开样式，设置 ARIA */
 function openDialog() {
   dialog.classList.add("openedDialog");
   dialog.classList.remove("closedDialog");
-  dialog_mask.classList.remove("closed_mask");
+  mask.classList.remove("closed_mask");
   entry.ariaExpanded = true;
 }
 
+/** 关闭弹窗，设置关闭样式，设置 ARIA */
 function closeDialog() {
   dialog.classList.remove("openedDialog");
   dialog.classList.add("closedDialog");
-  dialog_mask.classList.add("closed_mask");
+  mask.classList.add("closed_mask");
   entry.ariaExpanded = false;
 }
--- examples/run-start/missions/tabs.js
+++ examples/run-start/missions/tabs.js
@@ -1,17 +1,9 @@
-
-const tabs = document.getElementById("tab_list");
-
 const tabList = ["#tab_1", "#tab_2", "#tab_3", "#tab_4", "#tab_5"];
-const tabsBagel = focusBagel(tabs, tabList, {
-  onClick: onFocus,
-  next: {
-    key: e => e.key === "ArrowRight",
-    on: onFocus
-  },
-  prev: {
-    key: e => e.key === "ArrowLeft",
-    on: onFocus
-  },
+
+// 4～26 行为焦点管理的部分，管理了焦点的入口、出口，以及焦点在列表内的移动
+focusBagel("#tab_list", tabList, { // L:4
+  next: e => e.key === "ArrowRight",
+  prev: e => e.key === "ArrowLeft",
   exit: [{
     key: e => e.key === "Tab" && !e.shiftKey,
     target: "#tags_code",
@@ -30,10 +22,14 @@ const tabsBagel = focusBagel(tabs, tabList, {
   }],
   initialActive: 1,
   removeListenersEachExit: false,
-});
+  onMove,
+}); // L:26
 
-function onFocus({ prev, cur, prevI, curI }) {
-  if (curI === prevI) return;
+// 下面的函数和焦点无关，和样式或其它逻辑有关，这些代码在实际开发中，可以和上面的焦点部分分开，或者可以像本例中，把这些代码集成到焦点管理中
+
+/** 进行样式修改，当焦点移动时，本次聚焦添加样式，上一次聚焦移除样式，更新 ARIA */
+function onMove({ prev, cur, prevI, curI }) {
+  if (curI === prevI || curI === -1) return;
   (prev ?? document.getElementById("tab_1")).setAttribute("aria-selected", "false");
   cur.setAttribute("aria-selected", "true");
   const prevPanel = document.getElementById("tabpanel_" + (prevI + 1)) || document.getElementById("tabpanel_1");
